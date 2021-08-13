5f5239ceef45d9261e492665eb66c1b4f5449690
Aleksei Saunders <alekseisaunders@gmail.com>
2021-08-13 14:55:19 -0600

    added create new campground route and page

--- app.js
+++ app.js
@@ -19,6 +19,8 @@ db.once('open', () => {
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
+app.use(express.urlencoded({ extended: true }));
+
 app.get('/', (req, res, next) => {
   res.render('home');
 });
@@ -26,6 +28,21 @@ app.get('/', (req, res, next) => {
 app.get('/campgrounds', async (req, res, next) => {
   const campgrounds = await Campground.find({});
   res.render('campgrounds/index', { campgrounds });
+});
+
+app.get('/campgrounds/new', (req, res, next) => {
+  res.render('campgrounds/new');
+});
+
+app.post('/campgrounds', async (req, res, next) => {
+  const campground = new Campground(req.body.campground);
+  await campground.save();
+  res.redirect(`/campgrounds/${campground._id}`);
+});
+
+app.get('/campgrounds/:id', async (req, res, next) => {
+  const campground = await Campground.findById(req.params.id);
+  res.render('campgrounds/show', { campground });
 });
 
 app.listen(3000, () => {
