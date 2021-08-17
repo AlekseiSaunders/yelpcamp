24f88a361ed5635ed819e54f5353473a6007343e
Aleksei Saunders <alekseisaunders@gmail.com>
2021-08-16 18:41:22 -0600

    added ejs-mate and boilerplate view to all pages

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const mongoose = require('mongoose');
 const Campground = require('./models/campground');
 const methodOverride = require('method-override');
 const campground = require('./models/campground');
+const ejsMate = require('ejs-mate');
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp', {
   useNewUrlParser: true,
@@ -19,6 +20,7 @@ db.once('open', () => {
   console.log('Database connected');
 });
 
+app.engine('ejs', ejsMate);
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
