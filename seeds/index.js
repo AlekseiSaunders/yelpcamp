f30df4f777ad9d1f867e824e8ae794ebdc0669b6
Aleksei Saunders <alekseisaunders@gmail.com>
2021-08-16 20:36:29 -0600

    added styling, added images and descriptions, reseeded

--- seeds/index.js
+++ seeds/index.js
@@ -22,9 +22,14 @@ const seedDB = async () => {
   await Campground.deleteMany({});
   for (let i = 0; i < 50; i++) {
     const random1000 = Math.floor(Math.random() * 1000);
+    const price = Math.floor(Math.random() * 20 + 10);
     const camp = new Campground({
       title: `${sample(descriptors)} ${sample(places)}`,
       location: `${cities[random1000].city}, ${cities[random1000].state}`,
+      image: 'https://source.unsplash.com/collection/483251',
+      description:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Viverra nibh cras pulvinar mattis nunc sed blandit libero volutpat. Phasellus vestibulum lorem sed risus ultricies tristique. Fames ac turpis egestas maecenas pharetra convallis posuere morbi leo.',
+      price,
     });
     await camp.save();
   }
