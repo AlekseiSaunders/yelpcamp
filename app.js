4c99696bf2b7ea5430faccd06694ff3e5e17ff98
Aleksei Saunders <alekseisaunders@gmail.com>
2021-08-22 17:34:41 -0600

    added more advanced error handling

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const methodOverride = require('method-override');
 const campground = require('./models/campground');
 const ejsMate = require('ejs-mate');
 const catchAsync = require('./utilities/catchAsync');
+const ExpressError = require('./utilities/ExpressError');
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp', {
   useNewUrlParser: true,
@@ -48,6 +49,8 @@ app.get('/campgrounds/new', (req, res, next) => {
 app.post(
   '/campgrounds',
   catchAsync(async (req, res, next) => {
+    if (!req.body.campground)
+      throw new ExpressError('Invalid Campground Data', 400);
     const campground = new Campground(req.body.campground);
     await campground.save();
     res.redirect(`/campgrounds/${campground._id}`);
@@ -90,12 +93,18 @@ app.delete(
   })
 );
 
-// app.use((req, res) => {
-//   res.status(404).render('notfound');
-// });
+app.all('*', (req, res, next) => {
+  next(new ExpressError('Page Not Found', 404));
+});
 
 app.use((err, req, res, next) => {
-  res.send('Oh boy, something went wrong!');
+  const { statusCode = 500, message = 'Something went wrong' } = err;
+  res.status(statusCode);
+  if (statusCode === 404) {
+    res.status(404).render('notfound');
+  } else {
+    res.render('error');
+  }
 });
 
 app.listen(3000, () => {
