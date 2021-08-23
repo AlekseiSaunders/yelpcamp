c87f3c4726ce0723113bc5dcddaa6b114dba0fbc
Aleksei Saunders <alekseisaunders@gmail.com>
2021-08-23 11:02:45 -0600

    added more Joi functionality with error catching

--- app.js
+++ app.js
@@ -5,10 +5,11 @@ const morgan = require('morgan');
 const mongoose = require('mongoose');
 const Campground = require('./models/campground');
 const methodOverride = require('method-override');
-const campground = require('./models/campground');
+// const campground = require('./models/campground');
 const ejsMate = require('ejs-mate');
 const catchAsync = require('./utilities/catchAsync');
 const ExpressError = require('./utilities/ExpressError');
+const Joi = require('joi');
 const { error } = require('console');
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp', {
@@ -50,8 +51,21 @@ app.get('/campgrounds/new', (req, res, next) => {
 app.post(
   '/campgrounds',
   catchAsync(async (req, res, next) => {
-    if (!req.body.campground)
-      throw new ExpressError('Invalid Campground Data', 400);
+    // if (!req.body.campground)
+    //   throw new ExpressError('Invalid Campground Data', 400);
+    const campgroundSchema = Joi.object({
+      campground: Joi.object({
+        title: Joi.string().required(),
+        location: Joi.string().required(),
+        price: Joi.number().required().min(0),
+        image: Joi.string().required()
+      }).required(),
+    });
+    const { error } = campgroundSchema.validate(req.body);
+    if (error) {
+      const msg = error.details.map((el) => el.message).join(',');
+      throw new ExpressError(msg, 400);
+    }
     const campground = new Campground(req.body.campground);
     await campground.save();
     res.redirect(`/campgrounds/${campground._id}`);
