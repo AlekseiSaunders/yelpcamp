5304cb787d8085d588d942b48a4dd196760ff1d1
Aleksei Saunders <alekseisaunders@gmail.com>
2021-08-23 12:15:12 -0600

    moved joi validation to folder, refined schema for validation

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const methodOverride = require('method-override');
 const ejsMate = require('ejs-mate');
 const catchAsync = require('./utilities/catchAsync');
 const ExpressError = require('./utilities/ExpressError');
-const Joi = require('joi');
+const campgroundSchema = require('./validationSchemas/campgroundSchema.js');
 const { error } = require('console');
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp', {
@@ -32,6 +32,16 @@ app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
 app.use(morgan('dev'));
 
+const validateCampground = (req, res, next) => {
+  const { error } = campgroundSchema.validate(req.body);
+  if (error) {
+    const msg = error.details.map((el) => el.message).join(',');
+    throw new ExpressError(msg, 400);
+  } else {
+    next();
+  }
+};
+
 app.get('/', (req, res, next) => {
   res.render('home');
 });
@@ -50,22 +60,10 @@ app.get('/campgrounds/new', (req, res, next) => {
 
 app.post(
   '/campgrounds',
+  validateCampground,
   catchAsync(async (req, res, next) => {
     // if (!req.body.campground)
     //   throw new ExpressError('Invalid Campground Data', 400);
-    const campgroundSchema = Joi.object({
-      campground: Joi.object({
-        title: Joi.string().required(),
-        location: Joi.string().required(),
-        price: Joi.number().required().min(0),
-        image: Joi.string().required()
-      }).required(),
-    });
-    const { error } = campgroundSchema.validate(req.body);
-    if (error) {
-      const msg = error.details.map((el) => el.message).join(',');
-      throw new ExpressError(msg, 400);
-    }
     const campground = new Campground(req.body.campground);
     await campground.save();
     res.redirect(`/campgrounds/${campground._id}`);
@@ -90,6 +88,7 @@ app.get(
 
 app.put(
   '/campgrounds/:id',
+  validateCampground,
   catchAsync(async (req, res, next) => {
     const { id } = req.params;
     const campground = await Campground.findByIdAndUpdate(id, {
---
+++ validationSchemas/campgroundSchema.js
@@ -0,0 +1,12 @@
+const Joi = require('joi');
+
+const campgroundSchema = Joi.object({
+  campground: Joi.object({
+    title: Joi.string().required(),
+    location: Joi.string().required(),
+    price: Joi.number().required().min(0),
+    image: Joi.string().uri().required(),
+  }).required(),
+});
+
+module.exports = campgroundSchema;
