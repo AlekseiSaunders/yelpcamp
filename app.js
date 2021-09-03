a9616ad7aee7772c24880255f45024f9103347f6
Aleksei Saunders <alekseisaunders@gmail.com>
2021-09-03 09:45:20 -0600

    set cookie httpOnly: true

--- app.js
+++ app.js
@@ -39,8 +39,9 @@ const sessionConfig = {
   resave: false,
   saveUninitialized: true,
   cookie: {
+    httpOnly: true,
     expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
-    maxAge: 1000 * 60 * 60 * 24 * 7
+    maxAge: 1000 * 60 * 60 * 24 * 7,
   },
 };
 app.use(session(sessionConfig));
