275de9adc8a54793a55144ccf063a6d69fa0620f
Aleksei Saunders <alekseisaunders@gmail.com>
2021-09-10 15:07:42 -0600

    move middlewares from route files to middleware.js

--- utilities/middleware.js
+++ utilities/middleware.js
@@ -1,3 +1,9 @@
+const { id } = require('../validationSchemas/campgroundSchema');
+const ExpressError = require('./ExpressError');
+const campgroundSchema = require('../validationSchemas/campgroundSchema.js');
+const reviewSchema = require('../validationSchemas/reviewSchema.js');
+const Campground = require('../models/campground');
+
 const isLoggedIn = (req, res, next) => {
   if (!req.isAuthenticated()) {
     req.session.returnTo = req.originalUrl;
@@ -7,4 +13,37 @@ const isLoggedIn = (req, res, next) => {
   next();
 };
 
-module.exports = isLoggedIn;
+const validateCampground = (req, res, next) => {
+  const { error } = campgroundSchema.validate(req.body);
+  if (error) {
+    const msg = error.details.map((el) => el.message).join(',');
+    throw new ExpressError(msg, 400);
+  } else {
+    next();
+  }
+};
+
+const isAuthor = async (req, res, next) => {
+  const { id } = req.params;
+  const campground = await Campground.findById(id);
+  if (!campground.author.equals(req.user._id)) {
+    req.flash(
+      'error',
+      'Sorry, you need to be the author to perform that action'
+    );
+    return res.redirect(`/campgrounds/${id}`);
+  }
+  next();
+};
+
+const validateReview = (req, res, next) => {
+  const { error } = reviewSchema.validate(req.body);
+  if (error) {
+    const msg = error.details.map((el) => el.message).join(',');
+    throw new ExpressError(msg, 400);
+  } else {
+    next();
+  }
+};
+
+module.exports = { isLoggedIn, validateCampground, isAuthor, validateReview };
