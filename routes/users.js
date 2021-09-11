87a59b2c1300292f1aea3102d7b58e959a092630
Aleksei Saunders <alekseisaunders@gmail.com>
2021-09-11 06:01:58 -0600

    add user controller file and streamline user routes file

--- routes/users.js
+++ routes/users.js
@@ -4,34 +4,16 @@ const catchAsync = require('../utilities/catchAsync');
 const User = require('../models/user');
 const passport = require('passport');
 const isLoggedIn = require('../utilities/middleware');
+const users = require('../controllers/users');
 
-router.get('/register', (req, res, next) => {
-  res.render('users/register');
-});
+router.get('/register', users.renderRegister);
 
 router.post(
   '/register',
-  catchAsync(async (req, res, next) => {
-    try {
-      const { email, username, password } = req.body;
-      const user = new User({ email, username });
-      const registeredUser = await User.register(user, password);
-      console.log(registeredUser);
-      req.login(registeredUser, (err) => {
-        if (err) return next(err);
-        req.flash('success', 'Welcome to Yelp Camp!');
-        res.redirect('/campgrounds');
-      });
-    } catch (e) {
-      req.flash('error', e.message);
-      res.redirect('register');
-    }
-  })
+  catchAsync(users.registerUser)
 );
 
-router.get('/login', (req, res, next) => {
-  res.render('users/login');
-});
+router.get('/login', users.renderLogin);
 
 router.post(
   '/login',
@@ -39,21 +21,9 @@ router.post(
     failureFlash: true,
     failureRedirect: '/login',
   }),
-  (req, res, next) => {
-    req.flash(
-      'success',
-      'You have successfully logged in. Welcome back to Yelp Camp!'
-    );
-    const redirectUrl = req.session.returnTo || '/campgrounds';
-    delete req.session.returnTo;
-    res.redirect(redirectUrl);
-  }
+  users.loginUser
 );
 
-router.get('/logout', (req, res, next) => {
-  req.logout();
-  req.flash('success', 'You have logged out successfully.');
-  res.redirect('/campgrounds');
-});
+router.get('/logout', users.logoutUser);
 
 module.exports = router;
