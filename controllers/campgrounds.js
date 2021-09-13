d294b32e960283711606c4732e97bc7f3c89e299
Aleksei Saunders <alekseisaunders@gmail.com>
2021-09-12 18:54:45 -0600

    add upload of array of files for image use

--- controllers/campgrounds.js
+++ controllers/campgrounds.js
@@ -9,13 +9,16 @@ module.exports.renderNewForm = (req, res, next) => {
   res.render('campgrounds/new');
 };
 
-module.exports.createCamprground = async (req, res, next) => {
-  // if (!req.body.campground)
-  //   throw new ExpressError('Invalid Campground Data', 400);
-  req.flash('success', 'Successfully made a new campground.');
+module.exports.createCampground = async (req, res, next) => {
   const campground = new Campground(req.body.campground);
+  campground.images = req.files.map((f) => ({
+    url: f.path,
+    filename: f.filename,
+  }));
   campground.author = req.user._id;
   await campground.save();
+  console.log(campground);
+  req.flash('success', 'Successfully made a new campground!');
   res.redirect(`/campgrounds/${campground._id}`);
 };
 
--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -15,14 +15,13 @@ const upload = multer({ storage });
 router
   .route('/')
   .get(catchAsync(campgrounds.index))
-  // .post(
-  //   isLoggedIn,
-  //   validateCampground,
-  //   catchAsync(campgrounds.createCampground)
-  .post(upload.array('image'), (req, res, next) => {
-    console.log(req.body, req.files);
-    res.send('it Worked');
-  });
+  .post(
+    isLoggedIn,
+    upload.array('image'),
+    validateCampground,
+    catchAsync(campgrounds.createCampground)
+
+  );
 
 router.route('/new').get(isLoggedIn, campgrounds.renderNewForm);
 
