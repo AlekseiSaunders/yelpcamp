289b3cb7a1b166889aba9bf900fc0aaa67f8ddce
Aleksei Saunders <alekseisaunders@gmail.com>
2021-09-17 15:12:53 -0600

    add call  to delete images from cloudinary and from the mongo db

--- controllers/campgrounds.js
+++ controllers/campgrounds.js
@@ -1,4 +1,5 @@
 const Campground = require('../models/campground');
+const { cloudinary } = require('../cloudinary');
 
 module.exports.index = async (req, res, next) => {
   const campgrounds = await Campground.find({});
@@ -57,12 +58,19 @@ module.exports.renderEditForm = async (req, res, next) => {
 
 module.exports.updateCampground = async (req, res, next) => {
   const { id } = req.params;
-  console.log(req.body);
   const campground = await Campground.findByIdAndUpdate(id, {
     ...req.body.campground,
   });
   const imgs = req.files.map((f) => ({ url: f.path, filename: f.filename }));
   campground.images.push(...imgs);
+  if (req.body.deleteImages) {
+    for (let filename of req.body.deleteImages) {
+      await cloudinary.uploader.destroy(filename);
+    }
+    await campground.updateOne({
+      $pull: { images: { filename: { $in: req.body.deleteImages } } },
+    });
+  }
   await campground.save();
   req.flash('success', 'Successfully updated this campground.');
   res.redirect(`/campgrounds/${campground._id}`);
