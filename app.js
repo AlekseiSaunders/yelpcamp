eb5d5cb09dde56b5537f73875c699723ec67e6f1
Aleksei Saunders <alekseisaunders@gmail.com>
2021-10-01 14:51:09 -0600

    add connect-mongo package and config to store session in mongo db for memory use

--- app.js
+++ app.js
@@ -22,17 +22,29 @@ const campgroundRoutes = require('./routes/campgrounds');
 const reviewRoutes = require('./routes/reviews');
 const { any } = require('joi');
 const helmet = require('helmet');
-const dbUrl = process.env.DB_URL;
-
+const dbUrl = 'mongodb://localhost:27017/yelp-camp';
+const MongoDBStore = require('connect-mongo')(session);
+// process.env.DB_URL; eventual link to atlas DB
 // 'mongodb://localhost:27017/yelp-camp'; used for local hosting
-mongoose.connect('mongodb://localhost:27017/yelp-camp', {
+mongoose.connect(dbUrl, {
   useNewUrlParser: true,
   useCreateIndex: true,
   useUnifiedTopology: true,
   useFindAndModify: false,
 });
 
+const store = new MongoDBStore({
+  url: dbUrl,
+  secret: 'thischangesinproduction',
+  touchAfter: 24 * 60 * 60,
+});
+
+store.on('err', function (e) {
+  console.log('Session store error: ', e);
+});
+
 const sessionConfig = {
+  store: store,
   name: 'ycus',
   secret: 'thischangesinproduction',
   resave: false,
