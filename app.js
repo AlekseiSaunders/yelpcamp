11c94b39031b239d182136f78824311268862c01
Aleksei Saunders <alekseisaunders@gmail.com>
2021-10-01 10:24:57 -0600

    add AtlasDB functionality

--- app.js
+++ app.js
@@ -22,7 +22,9 @@ const campgroundRoutes = require('./routes/campgrounds');
 const reviewRoutes = require('./routes/reviews');
 const { any } = require('joi');
 const helmet = require('helmet');
+const dbUrl = process.env.DB_URL;
 
+// 'mongodb://localhost:27017/yelp-camp'; used for local hosting
 mongoose.connect('mongodb://localhost:27017/yelp-camp', {
   useNewUrlParser: true,
   useCreateIndex: true,
