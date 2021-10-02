793b0974e5a225650490b3714692e5f48c6a2f52
Aleksei Saunders <alekseisaunders@gmail.com>
2021-10-02 06:09:59 -0600

    attempt 3 to fix iOS Mobile map render

--- app.js
+++ app.js
@@ -82,26 +82,26 @@ app.use(flash());
 app.use(helmet());
 
 const scriptSrcUrls = [
-  'https://stackpath.bootstrapcdn.com/',
-  'https://api.tiles.mapbox.com/',
-  'https://api.mapbox.com/',
-  'https://kit.fontawesome.com/',
-  'https://cdnjs.cloudflare.com/',
+  'https://stackpath.bootstrapcdn.com',
+  'https://api.tiles.mapbox.com',
+  'https://api.mapbox.com',
+  'https://kit.fontawesome.com',
+  'https://cdnjs.cloudflare.com',
   'https://cdn.jsdelivr.net',
 ];
 const styleSrcUrls = [
-  'https://kit-free.fontawesome.com/',
-  'https://stackpath.bootstrapcdn.com/',
-  'https://api.mapbox.com/',
-  'https://api.tiles.mapbox.com/',
-  'https://fonts.googleapis.com/',
-  'https://use.fontawesome.com/',
+  'https://kit-free.fontawesome.com',
+  'https://stackpath.bootstrapcdn.com',
+  'https://api.mapbox.com',
+  'https://api.tiles.mapbox.com',
+  'https://fonts.googleapis.com',
+  'https://use.fontawesome.com',
   'https://cdn.jsdelivr.net',
 ];
 const connectSrcUrls = [
-  'https://api.mapbox.com/',
-  'https://*.tiles.mapbox.com/',
-  'https://events.mapbox.com/',
+  'https://api.mapbox.com',
+  'https://*.tiles.mapbox.com',
+  'https://events.mapbox.com',
 ];
 
 const fontSrcUrls = [];
@@ -121,7 +121,7 @@ app.use(
         'blob:',
         'data:',
         'https://res.cloudinary.com/dqfjdcset/',
-        'https://images.unsplash.com/',
+        'https://images.unsplash.com',
       ],
       fontSrc: ["'self'", ...fontSrcUrls],
     },
